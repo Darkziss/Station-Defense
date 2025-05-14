4cc41b26e0c8097290dd6390c5a85688290cd811
Darkziss <aserboom9@gmail.com>
2025-05-14 22:03:13 +0300

    Move CircleRenderer to root namespace

--- Assets/Scripts/CircleRenderer.cs
+++ Assets/Scripts/CircleRenderer.cs
@@ -1,61 +1,64 @@
 using UnityEngine;
 
-[RequireComponent(typeof(LineRenderer))]
-public class CircleRenderer : MonoBehaviour
+namespace StationDefense
 {
-    [SerializeField, Min(minRadius)] private float _radius;
-    [SerializeField, Min(minWidth)] private float _width = minWidth;
-    [SerializeField, Range(minSegmentCount, maxSegmentCount)] private int _segmentCount;
-    
-    private LineRenderer _lineRenderer;
-
-    private const float minRadius = 0.1f;
-
-    private const float minWidth = 1f;
-
-    private const int minSegmentCount = 1;
-    private const int maxSegmentCount = 128;
-
-    private const bool Loop = true;
-
-    private void OnValidate()
+    [RequireComponent(typeof(LineRenderer))]
+    public class CircleRenderer : MonoBehaviour
     {
-        if (_lineRenderer == null)
-            _lineRenderer = GetComponent<LineRenderer>();
+        [SerializeField, Min(minRadius)] private float _radius;
+        [SerializeField, Min(minWidth)] private float _width = minWidth;
+        [SerializeField, Range(minSegmentCount, maxSegmentCount)] private int _segmentCount;
 
-        DrawCircle();
-    }
+        private LineRenderer _lineRenderer;
 
-    private void DrawCircle()
-    {
-        _lineRenderer.loop = Loop;
+        private const float minRadius = 0.1f;
 
-        _lineRenderer.positionCount = _segmentCount;
-        _lineRenderer.widthMultiplier = _width;
+        private const float minWidth = 1f;
 
-        for (int i = 0; i < _segmentCount; i++)
+        private const int minSegmentCount = 1;
+        private const int maxSegmentCount = 128;
+
+        private const bool Loop = true;
+
+        private void OnValidate()
         {
-            const float Tau = Mathf.PI * 2f;
+            if (_lineRenderer == null)
+                _lineRenderer = GetComponent<LineRenderer>();
 
-            float progress = (float)i / _segmentCount;
-            float radian = progress * Tau;
-
-            Vector3 position = new()
-            {
-                x = Mathf.Sin(radian) * _radius,
-                y = Mathf.Cos(radian) * _radius
-            };
-
-            _lineRenderer.SetPosition(i, position);
+            DrawCircle();
         }
-    }
 
-    public void SetAlpha(float alpha)
-    {
-        Color color = _lineRenderer.startColor;
-        color.a = alpha;
+        private void DrawCircle()
+        {
+            _lineRenderer.loop = Loop;
 
-        _lineRenderer.startColor = color;
-        _lineRenderer.endColor = color;
+            _lineRenderer.positionCount = _segmentCount;
+            _lineRenderer.widthMultiplier = _width;
+
+            for (int i = 0; i < _segmentCount; i++)
+            {
+                const float Tau = Mathf.PI * 2f;
+
+                float progress = (float)i / _segmentCount;
+                float radian = progress * Tau;
+
+                Vector3 position = new()
+                {
+                    x = Mathf.Sin(radian) * _radius,
+                    y = Mathf.Cos(radian) * _radius
+                };
+
+                _lineRenderer.SetPosition(i, position);
+            }
+        }
+
+        public void SetAlpha(float alpha)
+        {
+            Color color = _lineRenderer.startColor;
+            color.a = alpha;
+
+            _lineRenderer.startColor = color;
+            _lineRenderer.endColor = color;
+        }
     }
 }

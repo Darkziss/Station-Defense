cb7175091256270c23b7dc2778555c740bf5b6b5
Darkziss <aserboom9@gmail.com>
2025-05-17 21:13:39 +0300

    Move GUI logic for enemy spawn to separate class

---
+++ Assets/Scripts/IMGUI/EnemySpawnWindow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace StationDefense.IMGUI
+{
+    public class EnemySpawnWindow : MonoBehaviour
+    {
+        [SerializeField] private EnemySpawner _enemySpawner;
+
+        private readonly Rect _windowRect = new(15f, 15f, 420, 200);
+        private readonly Rect _spawnButtonRect = new(30f, 30f, 150, 80);
+
+        private const string WindowContent = "Enemy Spawn";
+        private const string SpawnButtonContent = "Spawn Big Enemy";
+        
+        private void OnGUI()
+        {
+            GUI.Window(0, _windowRect, SpawnWindowFunction, WindowContent);
+        }
+
+        private void SpawnWindowFunction(int id)
+        {
+            if (GUI.Button(_spawnButtonRect, SpawnButtonContent))
+            {
+                _enemySpawner.SpawnBigEnemy();
+            }
+        }
+    }
+}

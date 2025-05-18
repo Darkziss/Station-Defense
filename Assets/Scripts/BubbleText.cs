c5d610386dd36667cf750f454ebee7963a601c70
Darkziss <aserboom9@gmail.com>
2025-05-18 22:19:29 +0300

    Add animations for BubbleText
    Create sequence of tweens that moves text up and fades out the text after a time

--- Assets/Scripts/BubbleText.cs
+++ Assets/Scripts/BubbleText.cs
@@ -1,20 +1,39 @@
-using System.Collections;
 using UnityEngine;
-using Pooling;
 using TMPro;
+using Pooling;
+using PrimeTween;
 
 namespace StationDefense
 {
     public class BubbleText : MonoBehaviour
     {
+        [SerializeField] private RectTransform _rectTransform;
         [SerializeField] private TMP_Text _text;
 
-        private readonly WaitForSeconds _disableDelay = new(DisableDelay);
+        private readonly WaitForSeconds _disableDelay = new(FadeOutDelay);
 
-        private const float DisableDelay = 0.8f;
+        private readonly TweenSettings _positionYAnimationSettings = new(PositionYDuration, 
+            ease: PositionYEase);
+
+        private readonly TweenSettings<float> _fadeOutSettings = new(0f, FadeOutDuration,
+            ease: FadeOutEase, startDelay: FadeOutDelay);
+
+        private float EndYPosition => _rectTransform.position.y + OffsetY;
+
+        private const float OffsetY = 0.3f;
+
+        private const float PositionYDuration = 0.3f;
+        private const Ease PositionYEase = Ease.Linear;
+
+        private const float FadeOutDuration = 0.5f;
+        private const Ease FadeOutEase = Ease.Linear;
+        private const float FadeOutDelay = 0.8f;
 
         private void OnValidate()
         {
+            if (_rectTransform == null)
+                _rectTransform = GetComponent<RectTransform>();
+
             if (_text == null)
                 _text = GetComponent<TMP_Text>();
         }
@@ -25,14 +44,10 @@ namespace StationDefense
             _text.color = color;
             _text.fontMaterial = material;
 
-            StartCoroutine(DisableWithDelay());
-        }
-
-        private IEnumerator DisableWithDelay()
-        {
-            yield return _disableDelay;
-
-            Disable();
+            Sequence.Create()
+                .Chain(Tween.PositionY(_rectTransform, EndYPosition, _positionYAnimationSettings))
+                .Chain(Tween.Alpha(_text, _fadeOutSettings))
+                .OnComplete(Disable);
         }
 
         private void Disable()
